d119eddcf8c28798d843a0d2f59ba265c75ca78e
Dirk Hoffmann <dirk.hoffmann@me.com>
2019-09-05 09:06:46 +0200

    Updated inv tests

--- Denise/BPU/inv1/inv1.s
+++ Denise/BPU/inv1/inv1.s
@@ -66,136 +66,152 @@ copper:
 	dc.w    $118,$000    ; BPL5DAT
 	dc.w    $11A,$000    ; BPL6DAT
 
-	; First color block
+	; First color block 
 	dc.w	$3001,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$30D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
 	dc.w	$3101,$FFFE  ; WAIT 
-	dc.w    BPLCON0,(7<<12)|$200 ; Invalid value 7
+	dc.w    $11A,$000    ; BPL6DAT
+	dc.w    $118,$000    ; BPL5DAT
+	dc.w    $116,$000    ; BPL4DAT
+	dc.w    $114,$000    ; BPL3DAT
+	dc.w    $112,$000    ; BPL2DAT
+	dc.w    $110,$000    ; BPL1DAT
+	dc.w    BPLCON0,(7<<12)|$200  ; Enable 7 bitplanes in lores mode
 
 	dc.w	$3801,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$38D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$3901,$FFFE  ; WAIT 
-	dc.w    BPLCON0,(4<<12)|$200 
+	dc.w	$3981,$FFFE  ; WAIT 
+	dc.w    $11A,$AAA    ; BPL6DAT
 
 	dc.w	$4001,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$40D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$4101,$FFFE  ; WAIT 
-	dc.w    BPLCON0,(7<<12)|$200 ; Invalid value 7
+	dc.w	$4181,$FFFE  ; WAIT 
+	dc.w    $118,$999    ; BPL5DAT
 
 	dc.w	$4801,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$48D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$4901,$FFFE  ; WAIT 
-	dc.w    $118,$AAA    ; BPL5DAT
+	dc.w	$4981,$FFFE  ; WAIT 
+	dc.w    $116,$E38    ; BPL4DAT
 
 	dc.w	$5001,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$50D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$5101,$FFFE  ; WAIT 
-	dc.w    $11A,$BBB    ; BPL6DAT
+	dc.w	$5181,$FFFE  ; WAIT 
+	dc.w    $114,$F0F    ; BPL3DAT
 
 	dc.w	$5801,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$58D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$5901,$FFFE  ; WAIT 
-	dc.w    BPLCON0,(4<<12)|$200 
-	dc.w    $118,$000    ; BPL5DAT
-	dc.w    $11A,$000    ; BPL6DAT
+	dc.w	$5981,$FFFE  ; WAIT 
+	dc.w    $112,$FC0    ; BPL2DAT
 
-  	; Second color block
+  	; Second color block 
 	dc.w	$7001,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$70D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$7101,$FFFE  ; WAIT 
-	dc.w    BPLCON0, (8<<12)|$200 
+	dc.w    $11A,$000    ; BPL6DAT
+	dc.w    $118,$000    ; BPL5DAT
+	dc.w    $116,$000    ; BPL4DAT
+	dc.w    $114,$000    ; BPL3DAT
+	dc.w    $112,$000    ; BPL2DAT
+	dc.w    $110,$000    ; BPL1DAT
+	dc.w	$7181,$FFFE  ; WAIT 
+	dc.w    BPLCON0, (15<<12)|$200  ; Enable 7 bitplanes in hires mode
 
 	dc.w	$7801,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$78D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$7901,$FFFE  ; WAIT 
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w	$7981,$FFFE  ; WAIT 
+	dc.w    $11A,$AAA    ; BPL6DAT
 
 	dc.w	$8001,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$80D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$8101,$FFFE  ; WAIT 
-	dc.w    BPLCON0, (9<<12)|$200 
+	dc.w	$8181,$FFFE  ; WAIT 
+	dc.w    $118,$999    ; BPL5DAT
 
 	dc.w	$8801,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$88D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$8801,$FFFE  ; WAIT 
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w	$8881,$FFFE  ; WAIT 
+	dc.w    $116,$E38    ; BPL4DAT
 
 	dc.w	$9001,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$90D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$9101,$FFFE  ; WAIT 
-	dc.w    BPLCON0, (10<<12)|$200 
+	dc.w	$9181,$FFFE  ; WAIT 
+	dc.w    $114,$F0F    ; BPL3DAT
 
 	dc.w	$9801,$FFFE  ; WAIT 
 	dc.w	COLOR00, $F00
 	dc.w	$98D9,$FFFE  ; WAIT 
 	dc.w	COLOR00, $000
-	dc.w	$9901,$FFFE  ; WAIT 
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w	$9981,$FFFE  ; WAIT 
+	dc.w    $112,$FC0    ; BPL2DAT
 
 	; Third color block
 	dc.w    $B801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $B8D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $B901,$FFFE  ; WAIT
-	dc.w    BPLCON0, (11<<12)|$200
+	dc.w    $B981,$FFFE  ; WAIT
+	dc.w    $11A,$000    ; BPL6DAT
+	dc.w    $118,$000    ; BPL5DAT
+	dc.w    $116,$000    ; BPL4DAT
+	dc.w    $114,$000    ; BPL3DAT
+	dc.w    $112,$000    ; BPL2DAT
+	dc.w    $110,$000    ; BPL1DAT
+	dc.w    BPLCON0, (14<<12)|$200  ; Enable 6 bitplanes in hires mode
 
 	dc.w    $C001,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $C0D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $C101,$FFFE  ; WAIT
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w    $C181,$FFFE  ; WAIT
+	dc.w    $11A,$AAA    ; BPL6DAT
 
 	dc.w    $C801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $C8D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $C901,$FFFE  ; WAIT
-	dc.w    BPLCON0, (12<<12)|$200
+	dc.w    $C981,$FFFE  ; WAIT
+	dc.w    $118,$999    ; BPL5DAT
 
 	dc.w    $D001,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $D0D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $D101,$FFFE  ; WAIT
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w    $D181,$FFFE  ; WAIT
+	dc.w    $116,$E38    ; BPL4DAT
 
 	dc.w    $D801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $D8D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $D901,$FFFE  ; WAIT
-	dc.w    BPLCON0, (13<<12)|$200
+	dc.w    $D981,$FFFE  ; WAIT
+	dc.w    $114,$F0F    ; BPL3DAT
 
 	dc.w    $E001,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $E0D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $E101,$FFFE  ; WAIT
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w    $E181,$FFFE  ; WAIT
+	dc.w    $112,$FC0    ; BPL2DAT
 
 	dc.w    $E801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
@@ -209,43 +225,49 @@ copper:
 	dc.w	COLOR00, $F00
 	dc.w    $00D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $0101,$FFFE  ; WAIT
-	dc.w    BPLCON0, (14<<12)|$200
+	dc.w    $0181,$FFFE  ; WAIT
+	dc.w    $11A,$000    ; BPL6DAT
+	dc.w    $118,$000    ; BPL5DAT
+	dc.w    $116,$000    ; BPL4DAT
+	dc.w    $114,$000    ; BPL3DAT
+	dc.w    $112,$000    ; BPL2DAT
+	dc.w    $110,$000    ; BPL1DAT
+	dc.w    BPLCON0, (13<<12)|$200 ; Enable 5 bitplanes in hires mode
 
 	dc.w    $0801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $08D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $0901,$FFFE  ; WAIT
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w    $0981,$FFFE  ; WAIT
+	dc.w    $11A,$AAA    ; BPL6DAT
 
 	dc.w    $1001,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $10D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $1101,$FFFE  ; WAIT
-	dc.w    BPLCON0, (15<<12)|$200
+	dc.w    $1181,$FFFE  ; WAIT
+	dc.w    $118,$999    ; BPL5DAT
 
 	dc.w    $1801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $18D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $1901,$FFFE  ; WAIT
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w    $1981,$FFFE  ; WAIT
+	dc.w    $116,$E38    ; BPL4DAT
 
 	dc.w    $2001,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $20D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $2101,$FFFE  ; WAIT
-	dc.w    BPLCON0, (7<<12)|$200
+	dc.w    $2181,$FFFE  ; WAIT
+	dc.w    $114,$F0F    ; BPL3DAT
 
 	dc.w    $2801,$FFFE  ; WAIT
 	dc.w	COLOR00, $F00
 	dc.w    $28D9,$FFFE  ; WAIT
 	dc.w	COLOR00, $000
-	dc.w    $2901,$FFFE  ; WAIT
-	dc.w    BPLCON0, (4<<12)|$200
+	dc.w    $2981,$FFFE  ; WAIT
+	dc.w    $112,$FC0    ; BPL2DAT
 
 	dc.l	$fffffffe
 
