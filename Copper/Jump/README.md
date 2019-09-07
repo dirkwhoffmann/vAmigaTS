8771d2c3a787f10d0cfba1ffa5b39e4908d204c6
Dirk Hoffmann <dirk.hoffmann@me.com>
2019-09-07 16:14:15 +0200

    Adding more Copper jump tests

--- Copper/Jump/README.md
+++ Copper/Jump/README.md
@@ -6,5 +6,13 @@ This test suite verfies particular aspects of the JUMP command (by writing into 
 
 At a certain position, Copper list 1 jumps to Copper list 2 which repeats itself. To visualize timing, the background color is alternated continously in the second Copper list.
 
+#### jump2 and jump3, jump3a
+
+Test jump2 triggers a Copper interrupt and executes a WAIT command afterwards. In the interrupt handler, the Copper location register (COP1LC) is written to. The test image reveals that writing to this register doesn't wake up the Copper nor does it affect the Copper's program counter. To let the change take effect, COPJMP1 needs to be written to which is done in jump3.
+
+#### jump4 and jump5
+
+Similar to jump3. The test try to visualize the timing when the Copper is woken up by a write to COPJMP1. For visualization, the background color is changed in the interrupt handler right before COPJMP1 is written to. Furthermore, the background color is changed three times right at the start of the Copper list. Test jump5 changes the background color in the interrupt handler *after* COPJMP1 is written to. It shows that the CPU freezes for a longer time after writing to COPJMP1.
+
 
 Dirk Hoffmann, 2019
---
+++ Copper/Jump/jump5/jump5.s
@@ -0,0 +1,109 @@
+	include "../../../../include/registers.i"
+	include "hardware/dmabits.i"
+	include "hardware/intbits.i"
+	
+CIAAPRA             equ $BFE001	
+CIABPRB             equ $BFD100	
+LVL2_INT_VECTOR		equ $68
+LVL3_INT_VECTOR		equ $6c
+SCREEN_WIDTH_BYTES	equ (320/8)
+SCREEN_BIT_DEPTH	equ 5
+	
+entry:	
+	lea	level3InterruptHandler(pc),a3
+ 	move.l	a3,LVL3_INT_VECTOR
+
+	; Install copper list and enable DMA
+	lea CUSTOM,a1
+	lea	copper(pc),a0
+	move.l	a0,COP1LC(a1)
+	move.w  COPJMP1(a1),d0
+	move.w  #(DMAF_SETCLR!DMAF_COPPER!DMAF_RASTER!DMAF_MASTER),dmacon(a1)
+
+	; Enable Copper interrupt
+	move.w	#$8010,INTENA(a1)
+	
+.mainLoop:
+
+	bra.b	.mainLoop
+
+level3InterruptHandler:
+	movem.l	d0-a6,-(sp)
+
+.checkVerticalBlank:
+	lea	CUSTOM,a5
+	move.w	INTREQR(a5),d0
+	and.w	#INTF_VERTB,d0	
+	beq.w	.checkCopper
+
+.verticalBlank:
+	move.w	#INTF_VERTB,INTREQ(a5)	; Clear interrupt bit	
+	move.w  #$222,COLOR00(a5)       ; Clear background color
+
+.resetBitplanePointers:
+	lea	bitplanes(pc),a1
+	lea     BPL1PTH(a5),a2
+	moveq	#SCREEN_BIT_DEPTH-1,d0
+
+.bitplaneloop:
+	move.l	a1,(a2)
+	lea	SCREEN_WIDTH_BYTES(a1),a1 ; bit plane data is interleaved
+	addq	#4,a2
+	dbra	d0,.bitplaneloop
+	
+.checkCopper:
+	lea	CUSTOM,a5
+	move.w	INTREQR(a5),d0
+	and.w	#INTF_COPER,d0	
+	beq.s	.interruptComplete
+
+.copperInterrupt:
+
+	; Reset the Copper program counter
+	lea CUSTOM,a1
+	lea	copper(pc),a0
+	move.l	a0,COP1LC(a1)
+
+	move.w	#INTF_COPER,INTREQ(a5)	; clear interrupt bit	
+	
+.interruptComplete:
+	move.w	#$70,INTREQ(a5)
+	movem.l	(sp)+,d0-a6
+	move.w  #$0,$DFF088   ; COPJMP1
+	move.w  #$000,$DFF180 ; COLOR00
+	rte
+
+copper:
+	dc.w    COLOR00,$F00
+	dc.w    COLOR00,$FF0
+	dc.w    COLOR00,$22F
+
+	dc.w    DIWSTRT,$2c81
+	dc.w	DIWSTOP,$2cc1
+	dc.w	BPLCON0,(5<<12)|$200 ; set color depth and enable COLOR
+	dc.w	BPL1MOD,SCREEN_WIDTH_BYTES*SCREEN_BIT_DEPTH-SCREEN_WIDTH_BYTES
+	dc.w	BPL2MOD,SCREEN_WIDTH_BYTES*SCREEN_BIT_DEPTH-SCREEN_WIDTH_BYTES
+ 
+ 	include	"image-copper-list-cropped.s"
+
+	dc.w    $6451, $FFFE         ; WAIT
+	dc.w    $6461, $FFFF         ; SKIP
+	dc.w 	INTREQ,$8010         ; Trigger Copper interrupt 
+	dc.w    COLOR00,$CCC
+
+	dc.w    $AC51, $FFFE         ; WAIT
+	dc.w    $AC61, $FFFF         ; SKIP
+	dc.w 	INTREQ,$8010         ; Trigger Copper interrupt 
+	dc.w    COLOR00,$444
+
+	dc.w    $F651, $FFFE         ; WAIT
+	dc.w    $F661, $FFFF         ; SKIP
+	dc.w 	INTREQ,$8010         ; Trigger Copper interrupt 
+	dc.w    COLOR00,$888
+
+	dc.w	$ffdf,$fffe          ; Cross vertical boundary
+	dc.l	$fffffffe
+
+bitplanes:
+	incbin	"out/image.bin"
+	
