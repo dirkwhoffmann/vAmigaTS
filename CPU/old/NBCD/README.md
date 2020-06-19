81d77469854892601be65e7fb9e4e6e0c177e9c7
Dirk Hoffmann <dirk.hoffmann@me.com>
2020-06-19 11:04:42 +0200

    Uploading

---
+++ CPU/old/NBCD/README.md
@@ -0,0 +1,23 @@
+## Objective
+
+Verify properties of the NBCD intruction.
+
+#### nbcd1
+
+Draws two 16 x 16 squares. In each square, the NBCD command is run for all 256 possible source values. The left square has the X flag cleared and the right square set. Each 8 x 8 pixel box shows the bit pattern of value computed by the instruction under test.
+
+#### nbcd2 
+
+Displays the value of the N flag after NBCD has been executed. 
+The N flag is of special interest, because it's value is described as undefined in the reference manual.
+
+#### nbcd3
+
+Displays the value of the V flag after NBCD has been executed. 
+The V flag is of special interest, because it's value is described as undefined in the reference manual.
+
+#### nbcd4
+
+Displays the value of the X,Z, and V flag after NBCD has been executed. 
+
+Dirk Hoffmann, 2019
---
+++ CPU/old/SUBQ/subq1/subq1.s
@@ -0,0 +1,220 @@
+	include "../../../../include/registers.i"
+	include "hardware/dmabits.i"
+	include "hardware/intbits.i"
+	
+CIAAPRA             equ $BFE001	
+CIABPRB             equ $BFD100	
+LVL1_INT_VECTOR		equ $64
+LVL2_INT_VECTOR		equ $68
+LVL3_INT_VECTOR		equ $6c
+LVL4_INT_VECTOR		equ $70
+LVL5_INT_VECTOR		equ $74
+LVL6_INT_VECTOR		equ $78
+SCREEN_WIDTH_BYTES	equ (320/8)
+SCREEN_BIT_DEPTH	equ 5
+	
+entry:	
+
+	; Disable CIA interrupts
+	move.b  #$7F,$BFDD00  ; CIA B
+	move.b  #$7F,$BFED01  ; CIA A
+
+	; Install interrupt handlers
+	lea	level1InterruptHandler(pc),a3
+ 	move.l	a3,LVL1_INT_VECTOR
+	lea	level2InterruptHandler(pc),a3
+ 	move.l	a3,LVL2_INT_VECTOR
+	lea	level3InterruptHandler(pc),a3
+ 	move.l	a3,LVL3_INT_VECTOR
+	lea	level4InterruptHandler(pc),a3
+ 	move.l	a3,LVL4_INT_VECTOR
+	lea	level5InterruptHandler(pc),a3
+ 	move.l	a3,LVL5_INT_VECTOR
+	lea	level6InterruptHandler(pc),a3
+ 	move.l	a3,LVL6_INT_VECTOR
+
+	; Install copper list
+	lea CUSTOM,a1
+	lea	copper(pc),a0
+	move.l	a0,COP1LC(a1)
+	move.w  COPJMP1(a1),d0
+
+	; Enable DMA
+	; move.w  #(DMAF_SETCLR!DMAF_COPPER!DMAF_RASTER!DMAF_MASTER),dmacon(a1)
+	move.w  #(DMAF_SETCLR!DMAF_COPPER!DMAF_MASTER),dmacon(a1)
+	
+.mainLoop:
+	; ds.w 500,$4E71 ; NOP field
+	bra.s	.mainLoop
+
+level1InterruptHandler:
+
+	move.w  #$F00,$DFF180
+	move.w  #$3FFF,$DFF09C ; Acknowledge
+	moveq   #$00,D0        ; Setup
+	move.w  #$FF0,$DFF180
+	ds.w 8,$4E71 ; NOP (4 cycles each)
+	move.w  #$000,$DFF180
+	rte
+
+level2InterruptHandler:
+
+	move.w  #$F00,$DFF180
+	move.w  #$3FFF,$DFF09C ; Acknowledge
+	moveq   #$00,D0        ; Setup
+	move.w  #$FF0,$DFF180
+	ds.w 16,$4E71 ; NOP (4 cycles each)
+	move.w  #$000,$DFF180
+	rte
+
+level3InterruptHandler:
+
+	move.w  #$F00,$DFF180
+	move.w  #$3FFF,$DFF09C ; Acknowledge
+	moveq   #$00,D0        ; Setup
+	move.w  #$FF0,$DFF180
+	subq.b  #1,D0 ; 1
+	subq.b  #2,D0 ; 2
+	subq.b  #3,D0 ; 3
+	subq.b  #4,D0 ; 4
+	subq.b  #5,D0 ; 5
+	subq.b  #6,D0 ; 6
+	subq.b  #7,D0 ; 7
+	subq.b  #7,D0 ; 8
+	move.w  #$000,$DFF180
+	rte
+
+level4InterruptHandler:
+
+	move.w  #$F00,$DFF180
+	move.w  #$3FFF,$DFF09C ; Acknowledge
+	moveq   #$00,D0        ; Setup
+	move.w  #$FF0,$DFF180
+	subq.w  #1,D0 ; 1
+	subq.w  #2,D0 ; 2
+	subq.w  #3,D0 ; 3
+	subq.w  #4,D0 ; 4
+	subq.w  #5,D0 ; 5
+	subq.w  #6,D0 ; 6
+	subq.w  #7,D0 ; 7
+	subq.w  #7,D0 ; 8
+	move.w #$000,$DFF180
+	rte
+
+level5InterruptHandler:
+
+	move.w  #$F00,$DFF180
+	move.w  #$3FFF,$DFF09C ; Acknowledge
+	moveq   #$00,D0        ; Setup
+	move.w  #$FF0,$DFF180
+	subq.l  #1,D0 ; 1
+	subq.l  #2,D0 ; 2
+	subq.l  #3,D0 ; 3
+	subq.l  #4,D0 ; 4
+	subq.l  #5,D0 ; 5
+	subq.l  #6,D0 ; 6
+	subq.l  #7,D0 ; 7
+	subq.l  #7,D0 ; 8
+	move.w  #$000,$DFF180
+	rte
+
+level6InterruptHandler:
+	move.w  #$F00,$DFF180
+	move.w  #$3FFF,$DFF09C ; Acknowledge
+	movea.l #$00,A0        ; Setup
+	move.w  #$FF0,$DFF180
+	subq.w  #1,A0 ; 1
+	subq.w  #2,A0 ; 2
+	subq.w  #3,A0 ; 3
+	subq.w  #4,A0 ; 4
+	subq.w  #5,A0 ; 5
+	subq.w  #6,A0 ; 6
+	subq.w  #7,A0 ; 7
+	subq.w  #7,A0 ; 8
+	move.w  #$000,$DFF180
+	rte
+
+copper:
+	;dc.w    DIWSTRT,$2c81
+	;dc.w	DIWSTOP,$2cc1
+	dc.w	BPLCON0,(0<<12)|$200 ; Disable all bitplanes and enable COLOR
+	;dc.w	BPL1MOD,SCREEN_WIDTH_BYTES*SCREEN_BIT_DEPTH-SCREEN_WIDTH_BYTES
+	;dc.w	BPL2MOD,SCREEN_WIDTH_BYTES*SCREEN_BIT_DEPTH-SCREEN_WIDTH_BYTES
+
+	dc.w    $3F39, $FFFE         ; WAIT
+	dc.w    COLOR00,$F00
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$0F0
+	dc.w    COLOR00,$000
+
+	dc.w	INTENA,$E89C         ; Enable interrupts
+
+	dc.w    $4139, $FFFE         ; Wait
+	dc.w    COLOR00,$00F
+	dc.w 	INTREQ,$8004         ; Level 1 interrupt
+
+	dc.w    $4339, $FFFE         ; Wait
+	dc.w    COLOR00,$00F
+	dc.w 	INTREQ,$8008         ; Level 2 interrupt
+
+	dc.w    $4539, $FFFE         ; Wait
+	dc.w    COLOR00,$00F
+	dc.w 	INTREQ,$8010         ; Level 3 interrupt
+
+	dc.w    $4739, $FFFE         ; Wait
+	dc.w    COLOR00,$00F
+	dc.w 	INTREQ,$8080         ; Level 4 interrupt
+
+	dc.w    $4939, $FFFE         ; Wait
+	dc.w    COLOR00,$00F
+	dc.w 	INTREQ,$8800         ; Level 5 interrupt
+
+	dc.w    $4B39, $FFFE         ; Wait
+	dc.w    COLOR00,$00F
+	dc.w 	INTREQ,$A000         ; Level 6 interrupt
+
+	dc.w	$ffdf,$fffe          ; Cross vertical boundary
+
+	dc.w	INTENA,$3FFF         ; Disable interrupts
+
+	dc.l	$fffffffe
+
+bitplanes:
+	ds.b 61440,$00
+	
