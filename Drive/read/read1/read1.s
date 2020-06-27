ec31301b327031203ea5dc523f093d09b757fcbb
Dirk Hoffmann <dirk.hoffmann@me.com>
2020-06-27 17:24:35 +0200

    Uploading

---
+++ Drive/read/read1/read1.s
@@ -0,0 +1,285 @@
+	include "../../../../include/registers.i"
+	include "hardware/dmabits.i"
+	include "hardware/intbits.i"
+	include "ministartup.s"
+
+CIAA_PRA            equ $BFE001	
+CIAA_PRB            equ $BFE101
+CIAA_DDRA           equ $BFE201
+CIAA_DDRB           equ $BFE301
+CIAA_TALO           equ $BFE401
+CIAA_TAHI           equ $BFE501
+CIAA_TBLO           equ $BFE601
+CIAA_TBHI           equ $BFE701
+CIAA_TODLO          equ $BFE801
+CIAA_TODMID         equ $BFE901
+CIAA_TODHI          equ $BFEA01
+CIAA_SDR            equ $BFEC01
+CIAA_ICR            equ $BFED01
+CIAA_CRA            equ $BFEE01
+CIAA_CRB            equ $BFEF01
+
+CIAB_PRA            equ $BFD000	
+CIAB_PRB            equ $BFD100
+CIAB_DDRA           equ $BFD200
+CIAB_DDRB           equ $BFD300
+CIAB_TALO           equ $BFD400
+CIAB_TAHI           equ $BFD500
+CIAB_TBLO           equ $BFD600
+CIAB_TBHI           equ $BFD700
+CIAB_TODLO          equ $BFD800
+CIAB_TODMID         equ $BFD900
+CIAB_TODHI          equ $BFDA00
+CIAB_SDR            equ $BFDC00
+CIAB_ICR            equ $BFDD00
+CIAB_CRA            equ $BFDE00
+CIAB_CRB            equ $BFDF00
+
+LVL1_INT_VECTOR		equ $64
+LVL2_INT_VECTOR	    equ $68
+LVL3_INT_VECTOR		equ $6c
+LVL4_INT_VECTOR		equ $70
+LVL5_INT_VECTOR		equ $74
+LVL6_INT_VECTOR		equ $78
+	
+MAIN:
+	; Load OCS base address into a1
+	lea CUSTOM,a1
+
+	; Disable all bitplanes 
+	move.w #$200,BPLCON0(a1)
+
+	; Disable CIA interrupts
+	move.b  #$7F,$BFDD00  ; CIA B
+	move.b  #$7F,$BFED01  ; CIA A
+
+	; Disable DMA and interrupts
+	move.w  #$7FFF,DMACON(a1)
+	move.w  #$7FFF,INTENA(a1)
+
+	; Install interrupt handlers
+	lea	irq1(pc),a2
+ 	move.l	a2,LVL1_INT_VECTOR
+	lea	irq3(pc),a2
+ 	move.l	a2,LVL3_INT_VECTOR
+	
+  	; Setup bitplane pointers
+	lea     bitplanes(pc),a2
+	lea     copper(pc),a3
+	moveq	#4,d0
+.bitplaneloop:
+	move.l 	a2,d1
+	move.w	d1,2(a3)
+	swap	d1
+	move.w  d1,6(a3)
+	addq	#8,a3
+	dbra	d0,.bitplaneloop
+
+	; Install copper list
+	lea	copper(pc),a0
+	move.l	a0,COP1LC(a1)
+	move.w  COPJMP1(a1),d0
+
+	; Enable DMA
+	move.w	#$8040,DMACON(a1)   ; Blitter DMA
+	move.w	#$8080,DMACON(a1)   ; Copper DMA
+	move.w	#$8010,DMACON(a1)   ; Disk DMA
+	; move.w	#$8100,DMACON(a1)   ; Bitplane DMA
+	move.w	#$8200,DMACON(a1)   ; DMA enable
+
+	; Prepare drive
+	lea	    diskdata,a0
+	move.w  #1024,d1
+.init:
+    move.w  #$F0,(a0)+
+	dbra    d1,.init
+
+	and.b   #$87,CIAB_PRB      ; Select all drives
+
+	;moveq   #40,d1
+	;jsr     moveHead           ; Move to track 40
+
+	jsr     MOTORON
+	and.b   #$F7,CIAB_PRB      ; Select df0
+
+    move.w  #$C012,INTENA(a1)  ; Enable interrupts
+
+;
+; Main loop
+;
+
+.mainloop:
+
+	jsr     synccpu
+	bra.s   .mainloop
+
+;
+; IRQ handlers
+;
+
+irq1:
+	move.w  #$F00,COLOR00(a1)
+	move.w  #$F0F,COLOR00(a1)
+	move.w  #$00F,COLOR00(a1)
+	move.w  #$FFF,COLOR00(a1)
+	move.w  #$F00,COLOR00(a1)
+	move.w  #$F0F,COLOR00(a1)
+	move.w  #$00F,COLOR00(a1)
+	move.w  #$FFF,COLOR00(a1)
+	move.w  #$000,COLOR00(a1)
+	move.w  #$0002,INTREQ(a1)
+	rte
+
+irq3:
+	move.w  #$888,COLOR00(a1)
+	move.w  #$0010,INTREQ(a1)
+
+	lea	    diskdata,a0    
+	move.l	a0,DSKPTH(a1)
+	move.w  #$4000,DSKLEN(a1)  ; Clear DMAEN
+	move.w  #$80FF,DSKLEN(a1)  
+	move.w  #$8000,DSKLEN(a1)  ; Start reading
+	
+	move.w  #$FF0,COLOR00(a1)
+	move.w  #$000,COLOR00(a1)
+	rte
+
+synccpu:
+	lea     VHPOSR(a1),a3     ; VHPOSR     
+
+	; Wait until we have reached the top of a frame
+.loop 
+	move.w  (a3),d2     
+	and     #$FF00,d2
+	cmp.w   #$3000,d2
+	bne     .loop
+	and     #1,VPOSR(a1)
+	bne     .loop
+
+	; Sync horizontally
+	move.w  #$F0F,COLOR00(a1)
+.synccpu1:
+	andi.w  #$F,(a3)          ; 16 cycles
+	bne     .synccpu1         ; 10 cycles
+	move.w  #$606,COLOR00(a1)
+.synccpu2:
+	andi.w  #$1F,(a3)         ; 16 cycles
+	bne     .synccpu2         ; 10 cycles
+	move.w  #$A0A,COLOR00(a1)
+.synccpu3:
+	andi.w  #$FF,(a3)         ; 16 cycles
+	nop                       ;  4 cycles
+	nop                       ;  4 cycles
+	nop                       ;  4 cycles
+	bne     .synccpu3         ; 10 cycles (if taken)
+
+	; Adust horizontally
+  	moveq #10,d2
+.adjust:
+    dbra d2,.adjust
+
+	; Sync vertically
+.synccpu4:
+	nop 
+	move.w  #$404,COLOR00(a1)
+	ds.w    96,$4E71          ; NOPs to keep the horizontal position in each iteration
+	move.w  (a3),d2     
+	move.w  #$F0F,COLOR00(a1)  
+	and     #$FF00,d2
+	cmp.w   #$4000,d2
+	bne     .synccpu4
+	move.w  #$000,COLOR00(a1)  
+	rts
+
+copper:
+	;; bitplane pointers must be first else poking addresses will be incorrect
+	dc.w	BPL1PTL,0
+	dc.w	BPL1PTH,0
+	dc.w	BPL2PTL,0
+	dc.w	BPL2PTH,0
+	dc.w	BPL3PTL,0
+	dc.w	BPL3PTH,0
+	dc.w	BPL4PTL,0
+	dc.w	BPL4PTH,0
+	dc.w	BPL5PTL,0
+	dc.w	BPL5PTH,0
+
+	; Draw reference lines
+	dc.w    $4E39, $FFFE         ; WAIT
+	dc.w    COLOR00,$F00       
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$FFF
+	dc.w    COLOR00,$000
+	dc.w    COLOR00,$0F0
+	dc.w    COLOR00,$000
+
+	dc.w	$5009, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$5819, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$6029, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$6839, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$7049, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$7859, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$8069, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	dc.w	$8879, $FFFE  
+	dc.w    INTREQ, $8010    ; Level 3 interrupt
+
+	; Cross vertical boundary
+	dc.w    $ffdf,$fffe 
+
+	dc.l    $fffffffe	
+
+	include "diskutil.s"
+
+bitplanes:
+	ds.b    61440,0
+
+diskdata:
+	ds.b    1025
