a7c61d081d0ffe6b6c74a178aaca936715ac27c5
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-10-29 11:34:38 +0200

    Uploading

---
+++ Blitter/timing/timing1l/timing1l.s
@@ -0,0 +1,12 @@
+BLIT_BLTCON0        equ $01CA
+BLIT_BLTCON1		equ $0001
+
+COL1                equ $6FB   ; Use highlighted color if channel A is enabled
+COL2                equ $6FB   ; Use highlighted color if channel B is enabled
+COL3                equ $6FB   ; Use highlighted color if channel C is enabled
+COL4                equ $BB6   ; Use highlighted color if channel D is enabled
+COL5                equ $6FB   ; Use highlighted color in fill mode
+COL6                equ $6FB   ; Use highlighted color in single dot mode
+COL7                equ $BB6   ; Use highlighted color in line mode
+
+	include "../timing.i"
---
+++ Blitter/timing/timing6/timing6.s
@@ -0,0 +1,12 @@
+BLIT_BLTCON0        equ $06CA
+BLIT_BLTCON1		equ $0000
+
+COL1                equ $6BF   ; Use highlighted color if channel A is enabled
+COL2                equ $BB6   ; Use highlighted color if channel B is enabled
+COL3                equ $BB6   ; Use highlighted color if channel C is enabled
+COL4                equ $6BF   ; Use highlighted color if channel D is enabled
+COL5                equ $6BF   ; Use highlighted color in fill mode
+COL6                equ $6BF   ; Use highlighted color in single dot mode
+COL7                equ $6BF   ; Use highlighted color in line mode
+
+	include "../timing.i"
