8a43a4ff34a9ba880f185364dc6993d03af63804
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-01-16 14:46:11 +0100

    Uploading

---
+++ Agnus/DMACON/resmod4/resmod4.s
@@ -0,0 +1,65 @@
+
+	include "../bpumod.i"
+
+copper:
+	COPPER
+	dc.w    DIWSTRT,$3a81
+	dc.w	DIWSTOP,$1ac1
+
+	dc.w	$3841,$FFFE  ; WAIT 
+	RULER
+
+
+	dc.w    $4061,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $4863,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $5065,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $5867,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $6069,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $686B,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $706D,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $786F,$FFFE 
+	dc.w    BPLCON0,$A200
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    $1FE,$0000      ; NOP
+	dc.w    BPLCON0,$2200
+
+	dc.w    $ffdf,$fffe  ; Cross vertical boundary
+	dc.w	$0641,$FFFE  ; WAIT 
+	RULER
+
+	dc.l	$fffffffe
