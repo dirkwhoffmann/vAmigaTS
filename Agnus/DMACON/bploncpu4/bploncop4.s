2e88644965601a2bf9818b5fdbe6895ed63ae42e
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-06-26 17:17:31 +0200

    Uploading

--- Agnus/DMACON/bploncpu4/bploncop4.s
+++
@@ -1,6 +0,0 @@
-DDFSTRTINI          equ $4A
-DDFSTOPINI			equ $AA
-
-COL  				equ $4F4
-
-	include "../bploncop.i"
---
+++ Agnus/DMACON/bploncpu4/bploncpu4.s
@@ -0,0 +1,7 @@
+DDFSTRTINI          equ $4A
+DDFSTOPINI			equ $AA
+
+COL  				equ $CC4
+IDLECOL  			equ $8AF
+
+	include "../bploncpu.i"
