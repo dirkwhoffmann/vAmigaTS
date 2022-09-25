934c1537e0d8a534c75bc288cd75ae0deeee37b1
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-09-25 23:38:34 +0200

    Uploading

---
+++ CPU/MMU30/30translate5l/30translate5l.s
@@ -0,0 +1,16 @@
+	include "../30translate.i"
+	include "../table_long.i"
+
+info: 
+    dc.b '30TRANSLATE5 Long: A->B->C->D->I->Mem', 0
+	even
+
+trap0:
+	; Patch the MMU table (nothing to do here)
+
+	; Enable the MMU
+	jsr setupTC
+
+	; Change background color by using the mirror space at $Axxxxx
+    move.w  #$0A0,$aff180
+    rte
