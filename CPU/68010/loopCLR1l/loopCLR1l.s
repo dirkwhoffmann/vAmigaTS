514b6262b205c7f0d6f04c1190b4160c2c38f9ba
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-10-22 12:41:02 +0200

    Uploading

---
+++ CPU/68010/loopCLR1l/loopCLR1l.s
@@ -0,0 +1,5 @@
+INSTRUCTION	MACRO
+        	clr.l	(a5)
+            ENDM
+
+	include "../loopmode.i"
---
+++ CPU/68010/loopCLR2w/loopCLR2w.s
@@ -0,0 +1,5 @@
+INSTRUCTION	MACRO
+        	clr.w	(a5)+
+            ENDM
+
+	include "../loopmode.i"
