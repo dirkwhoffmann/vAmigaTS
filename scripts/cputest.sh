df8a4e4d3430e63303f5e706fb733bbffc97795f
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-10-29 08:49:52 +0200

    Uploading regression scripts

---
+++ scripts/cputest.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Creates two script file for a standard CPU test
+# Dirk W. Hoffmann, 2022
+#
+# Usage: cputest <list of test directories>
+
+function createScript () {
+
+	NAME=${1%/}_$2
+	FILE=${NAME}.ini
+	ADF=/tmp/${NAME}.adf
+
+	echo "# Regression testing script for vAmiga" > $FILE
+	echo "# Dirk W. Hoffmann, $(date +'%Y')" >> $FILE
+	echo "" >> $FILE
+	echo "# Setup the test environment" >> $FILE
+	echo "regression setup A500_ECS_1MB /tmp/kick13.rom" >> $FILE
+	if [[ "$2" != "68000" ]]; then
+		echo "cpu set revision $2" >> $FILE
+	fi
+	echo "" >> $FILE
+	echo "# Run the test" >> $FILE
+	echo "regression run $ADF" >> $FILE
+	echo "wait 9 seconds" >> $FILE
+	echo "" >> $FILE
+	echo "# Exit with a screenshot" >> $FILE
+	echo "screenshot save $NAME" >> $FILE
+}
+
+for dir in "$@"
+do
+	if [[ -d $dir ]]; then
+		echo "Creating scripts for $dir"
+		cd $dir
+		createScript $dir 68000
+		createScript $dir 68010
+		cd ..
+	fi
+done
+
