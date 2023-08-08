0f3af0c39ca19380b87a4debdab7289c39f08430
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-08-08 20:53:38 +0200

    Fixing some FPU tests

--- FPU/fmove/fmove-fpcr2/fmove-fpcr2.s
+++ FPU/fmove/fmove-fpcr2/fmove-fpcr2.s
@@ -16,7 +16,7 @@ trap0:
     fmovem  fpsr,(a2)+
     fmovem  fpiar,(a2)+
     fmovem  fpcr/fpsr,(a2)+
-    fmovem  fpsr/fpiar,(a2)+
+    fmovem  fpcr/fpiar,(a2)+
     fmovem  fpsr/fpiar,(a2)+
     fmovem  fpcr/fpsr/fpiar,(a2)+
 
@@ -28,7 +28,7 @@ trap0:
     fmovem  fpsr,4(a2)
     fmovem  fpiar,8(a2)
     fmovem  fpcr/fpsr,12(a2)
-    fmovem  fpsr/fpiar,16(a2)
+    fmovem  fpcr/fpiar,16(a2)
     fmovem  fpsr/fpiar,20(a2)
     fmovem  fpcr/fpsr/fpiar,24(a2)
 
@@ -37,7 +37,7 @@ trap0:
     fmovem  fpsr,-(a2)
     fmovem  fpiar,-(a2)
     fmovem  fpcr/fpsr,-(a2)
-    fmovem  fpsr/fpiar,-(a2)
+    fmovem  fpcr/fpiar,-(a2)
     fmovem  fpsr/fpiar,-(a2)
     fmovem  fpcr/fpsr/fpiar,-(a2)
 
@@ -51,13 +51,13 @@ info:
 expected:
     dc.b    $00,$00,$AB,$C0,  $0C,$DA,$BC,$D8  ; 1
     dc.b    $CD,$AB,$CD,$AB,  $00,$00,$AB,$C0  ; 2
-    dc.b    $0C,$DA,$BC,$D8,  $0C,$DA,$BC,$D8  ; 3
+    dc.b    $0C,$DA,$BC,$D8,  $00,$00,$AB,$C0  ; 3
     dc.b    $CD,$AB,$CD,$AB,  $0C,$DA,$BC,$D8  ; 4
     dc.b    $CD,$AB,$CD,$AB,  $00,$00,$AB,$C0  ; 5
     dc.b    $0C,$DA,$BC,$D8,  $CD,$AB,$CD,$AB  ; 6
     dc.b    $00,$00,$56,$70,  $04,$56,$78,$10  ; 7
     dc.b    $56,$78,$12,$34,  $00,$00,$56,$70  ; 8
-    dc.b    $04,$56,$78,$10,  $04,$56,$78,$10  ; 9
+    dc.b    $00,$00,$56,$70,  $04,$56,$78,$10  ; 9
     dc.b    $00,$00,$56,$70,  $04,$56,$78,$10  ; 10
     dc.b    $56,$78,$12,$34,  $00,$00,$00,$00  ; 11
     dc.b    $00,$00,$00,$00,  $00,$00,$00,$00  ; 12
@@ -69,7 +69,7 @@ expected:
     dc.b    $00,$00,$00,$00,  $00,$00,$00,$00  ; 18
     dc.b    $00,$00,$56,$70,  $04,$56,$78,$10  ; 19
     dc.b    $56,$78,$12,$34,  $04,$56,$78,$10  ; 20
-    dc.b    $56,$78,$12,$34,  $04,$56,$78,$10  ; 21
+    dc.b    $56,$78,$12,$34,  $00,$00,$56,$70  ; 21
     dc.b    $56,$78,$12,$34,  $00,$00,$56,$70  ; 22
     dc.b    $04,$56,$78,$10,  $56,$78,$12,$34  ; 23
     dc.b    $04,$56,$78,$10,  $00,$00,$56,$70  ; 24
